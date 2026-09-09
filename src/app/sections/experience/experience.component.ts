54cab33c3bafe8fc174609499eea67b627966096
Mateus-Rocha1 <mateusprocha14@gmail.com>
2026-09-09 01:37:59 -0300

    section projetos

--- src/app/sections/experience/experience.component.ts
+++ src/app/sections/experience/experience.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface Experience {
+  cargo: string;
+  periodo: string;
+  local: string;
+  descricao: string;
+  historico?: { cargo: string; periodo: string }[];
+}
 
 @Component({
   selector: 'app-experience',
   templateUrl: './experience.component.html',
   styleUrls: ['./experience.component.scss']
 })
-export class ExperienceComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class ExperienceComponent {
+  experiencias: Experience[] = [
+    {
+      cargo: 'Desenvolvedor Frontend Júnior',
+      periodo: '08/25 até o momento',
+      local: 'Laboratório de Visão Computacional e Inteligência Artificial (VICIA)',
+      descricao: 'Desenvolvimento de projeto em parceria com o Ministério da Educação (MEC), incluindo a criação de telas, ajustes de componentes e integração com a camada de backend (APIs) para garantir o funcionamento completo da aplicação.'
+    },
+    {
+      cargo: 'UX Designer Pleno',
+      periodo: '09/25 até o momento',
+      local: 'Laboratório de Telemática, Acessibilidade e Projetos Educacionais (LTAP)',
+      descricao: 'Criação de fluxos, identidade visual e padrões; desenvolvimento de prototípos; colaboração com desenvolvedores; análise de requisitos, usabilidade e acessibilidade.',
+      historico: [
+        { cargo: 'UX Designer', periodo: '09/25 a 07/26' },
+        { cargo: 'UX Designer Pleno', periodo: '04/26 até o momento' }
+      ]
+    },
+    {
+      cargo: 'UX Designer',
+      periodo: '06/25 a 09/25',
+      local: 'Núcleo de Desenvolvimento de Software',
+      descricao: 'Análise de padrões; criação de fluxos; desenvolvimento de prototípos; colaboração com desenvolvedores e análise de usabilidade para melhoria contínua da interface.'
+    }
+  ];
 }
--- src/app/sections/projects/projects.component.ts
+++ src/app/sections/projects/projects.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface Project {
+  nome: string;
+  descricao: string;
+  imagem: string;
+  githubUrl: string;
+  tecnologias: string[];
+}
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss']
 })
-export class ProjectsComponent implements OnInit {
+export class ProjectsComponent {
+  projetos: Project[] = [
+    {
+      nome: 'RatingUp',
+      descricao: 'Plataforma web criada com o propósito de reduzir a lacuna no aprendizado de xadrez na população brasileira, tornando o ensino do jogo mais acessível, moderno e envolvente.',
+      imagem: 'assets/img/projects/ratingup.png',
+      githubUrl: 'https://github.com/SEU-USUARIO/ratingup',
+      tecnologias: ['Angular', 'Typescript', 'Java', 'Spring', 'MinIO', 'Docker', 'PostgreSQL', 'Swagger', 'RobotFramework', 'NGINX']
+    }
+    // adicione mais projetos aqui, seguindo o mesmo formato
+  ];
 
-  constructor() { }
+  indiceAtual = 0;
 
-  ngOnInit(): void {
+  anterior() {
+    this.indiceAtual = this.indiceAtual === 0
+      ? this.projetos.length - 1
+      : this.indiceAtual - 1;
   }
 
+  proximo() {
+    this.indiceAtual = this.indiceAtual === this.projetos.length - 1
+      ? 0
+      : this.indiceAtual + 1;
+  }
 }
